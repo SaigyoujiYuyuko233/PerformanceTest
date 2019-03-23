bea0bb7018c7e2be29ea25804bdc486a4f71f90f
SaigyoujiYuyuko233 <purple_guy@qq.com>
2019-03-23 21:20:51 +0800

    修复自动安装依赖库程序 #2

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ echo "This is the automic install compile dependent library program."
 echo "Make sure you can successfully run the command: go install"
 echo "Start the program..."
 
+# get
+go get github.com/gookit/color
+
+# install
 go install github.com/gookit/color
