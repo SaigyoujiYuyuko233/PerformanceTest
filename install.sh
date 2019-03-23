c5265666a1ecd08ea5be26264d17ff8be845d83a
SaigyoujiYuyuko233 <purple_guy@qq.com>
2019-03-23 21:17:19 +0800

    自动安装依赖库程序

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "This is the automic install compile dependent library program."
+echo "Make sure you can successfully run the command: go install"
+echo "Start the program in 3s"
+
+sleep 3;
+
+go install github.com/gookit/color
